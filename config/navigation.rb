aa62a7de90a13dd9ad2d40055b4bbc231b6e8ac3
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 14:06:05 -0300

    Add simple navigation menu bar

---
+++ config/navigation.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Configures your navigation
+SimpleNavigation::Configuration.run do |navigation|
+  # Define the primary navigation
+  navigation.items do |primary|
+    # Account menu
+    primary.item :account, 'Conta', icon: 'glyphicon glyphicon-user' do |sub|
+      sub.item :login, 'Login', new_user_session_path, icon: 'glyphicon glyphicon-log-in', :unless => lambda { user_signed_in? }
+      sub.item :logout, 'Logout', destroy_user_session_path, icon: 'glyphicon glyphicon-log-out', :if => lambda { user_signed_in? }
+      sub.item :my_data, 'Meus dados', edit_user_registration_path, icon: 'fa fa-suitcase', :if => lambda { user_signed_in? }
+    end
+
+    # Meat menu
+    primary.item :meats, 'Carnes', icon: 'fa fa-cc-visa', :if => lambda { user_signed_in? } do |sub|
+      sub.item :index, 'Listar', meats_path, icon: 'glyphicon glyphicon-th-list'
+      sub.item :creare, 'Cadastrar', new_meat_path, icon: 'fa fa-credit-card'
+    end
+
+    # Side Dish menu
+    primary.item :meats, 'Acompanhamentos', icon: 'fa fa-cc-visa', :if => lambda { user_signed_in? } do |sub|
+      sub.item :index, 'Listar', side_dishes_path, icon: 'glyphicon glyphicon-th-list'
+      sub.item :creare, 'Cadastrar', new_side_dish_path, icon: 'fa fa-credit-card'
+    end
+
+    # Orders menu
+    primary.item :meats, 'Pedidos', icon: 'fa fa-cc-visa', :if => lambda { user_signed_in? } do |sub|
+      sub.item :index, 'Listar', orders_path, icon: 'glyphicon glyphicon-th-list'
+      sub.item :creare, 'Cadastrar', new_order_path, icon: 'fa fa-credit-card'
+    end
+
+    primary.dom_id = 'menu-root'
+    primary.dom_class = 'nav navbar-nav'
+  end
+end
