5956f8cc7e417e17565364fc70966cca86e2493f
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 13:31:05 -0300

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
+
   resources :meats
   resources :side_dishes
   resources :orders
