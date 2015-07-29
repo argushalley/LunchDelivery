c0730809adbdbf772035302b43c4c3b8dcb6c588
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 12:31:10 -0300

    Add factory_girl and improve model specs

--- spec/models/side_dish_spec.rb
+++ spec/models/side_dish_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe SideDish, type: :model do
   it { is_expected.to have_many(:orders).through(:order_side_dishes) }
   it { is_expected.to validate_presence_of(:name) }
 
-  #it 'validates uniqueness of name' do
-  #  create(:side_dish)
-  #  validate_uniqueness_of(:name)
-  #end
+  it 'validates uniqueness of name' do
+    create(:side_dish)
+    validate_uniqueness_of(:name)
+  end
 end
