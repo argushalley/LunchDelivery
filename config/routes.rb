ef6fa5abec99f39f8ab77280ab0f431142e8943c
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 12:59:36 -0300

    Generate devise controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
   resources :meats
   resources :side_dishes
   resources :orders
