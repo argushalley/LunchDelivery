26b9e59449a8c62f37e67dd2e96f570cd8dc18d2
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 16:58:39 -0300

    Add error flash messages when trying to delete a relationship between order and side_dish/meat

--- app/models/side_dish.rb
+++ app/models/side_dish.rb
@@ -1,6 +1,17 @@
 class SideDish < ActiveRecord::Base
+  before_destroy :check_for_orders
+
   has_many :order_side_dishes
   has_many :orders, through: :order_side_dishes
 
   validates :name, presence: true, uniqueness: true
+
+  private
+
+  def check_for_orders
+    if orders.any?
+      errors[:base] << "cannot delete side dish that has already been ordered"
+      return false
+    end
+  end
 end
