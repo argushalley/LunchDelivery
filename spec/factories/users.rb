a90508a74c55de6953c9a2a8a84b7e785c0013c0
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-07-29 21:41:55 -0300

    Improve model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) {|n| "user#{n}@example.com"}
+    password 'password'
+    role :default
   end
 
+  factory :admin, class: User do
+    sequence(:email) {|n| "admin#{n}@example.com"}
+    password 'password'
+    role :admin
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   it { is_expected.to belong_to(:order).with_foreign_key(:order_id) }
+  it { is_expected.to validate_presence_of(:street) }
+  it { is_expected.to validate_presence_of(:number) }
 end
